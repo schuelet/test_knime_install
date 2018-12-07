a0159a4ead31e2107843b0e3409c97bc74170890
Miguel <miguel.de-alba-aparicio@bfr.bund.de>
2018-12-07 14:23:56 +0100

    Remove unused imports and path to KNIME

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -3,13 +3,9 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.Arrays;
 import java.util.List;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.fail;
 
 public class MyTest {
